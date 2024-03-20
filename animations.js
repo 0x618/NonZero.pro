d4bf9116e3ea2dd33c5b14c03984ded189c5a6e7
Michael Beck <michael.beck@nonzero.pro>
2024-03-20 16:07:28 -0500

    New placeholder animation for unpopulated pages.

--- animations.js
+++ animations.js
@@ -1,22 +1,36 @@
-let busy = false;
+let logoBusy = false;
+let termFired = false;
 function logoAnimation(text) {
-    if (!busy) {
-    busy = true;
-    event.target.innerHTML = '&lt;/&gt;';
-    innerText = ''
-    for (let i = 0; i < text.length; i++) {
-        setTimeout((e) => {
-        innerText += text[i];
-        e.innerHTML = '&lt;/' + innerText + '&gt;';
-        }, i * 100, event.target);
-    }
-    setTimeout(() => {
-        busy = false;
-    }, 600, busy);
+    if (!logoBusy) {
+        logoBusy = true;
+        event.target.innerHTML = '&lt;/&gt;';
+        innerText = ''
+        for (let i = 0; i < text.length; i++) {
+            setTimeout((e) => {
+                innerText += text[i];
+                e.innerHTML = '&lt;/' + innerText + '&gt;';
+            }, i * 100, event.target);
+        }
+        setTimeout(() => {
+            logoBusy = false;
+        }, 600, logoBusy);
     }
 }
 function logoReset(text) {
     setTimeout((e) => {
         e.target.innerHTML = '&lt;/&gt;';
     }, 500, event);
+}
+function terminalAnimation(text) {
+    if (!termFired) {
+        termFired = true;
+        term = document.getElementsByClassName('terminal')[0];
+        prefix = term.innerHTML;
+        for (let i = 0; i < text.length; i++) {
+            setTimeout((term) => {
+                prefix += text[i];
+                term.innerHTML = prefix;
+            }, i * 75, term);
+        }
+    }
 }
